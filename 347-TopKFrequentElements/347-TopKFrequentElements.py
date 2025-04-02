ac9fc2ac753ad0d6ce6acbb4d99c8c3d32a3b372
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-04-02 14:07:46 -0700

    File updated at 02/04/2025, 14:07:46

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,12 +1,27 @@
-# Last updated: 02/04/2025, 13:46:55
-
+# Last updated: 02/04/2025, 14:07:46
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # Brute force approach
         # Sort the elements and return the last k elements
        
-        count = Counter(nums)
-        sort_freq = sorted(count, key=count.get)
+        # count = Counter(nums)
+        # sort_freq = sorted(count, key=count.get)
         
-        return sort_freq[-k:]
+        # return sort_freq[-k:]
+
+        # Optimal approach
+        # Use a heap to store the elements and pop k elements from it
+
+        count = Counter(nums)
+        heap = []
+        for num, freq in count.items():
+            heap.append((-freq, num))
+        heapq.heapify(heap)
+        res = []
+        for i in range(k):
+            freq, num = heapq.heappop(heap)
+            res.append(num)
+        return res
+        
 
