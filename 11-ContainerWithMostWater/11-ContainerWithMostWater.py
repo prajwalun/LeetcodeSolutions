f04e99bbbeb50958cc969fd5221ac0e4d3cd2f7d
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-04-02 23:19:29 -0700

    Create file

---
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -0,0 +1,23 @@
+# Last updated: 02/04/2025, 23:19:29
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # Brute Force
+        # Find a way to calculate the water stored for each of the container length
+        # Store it in a result variable
+        # Update the result variable if we find a higher amount of water
+
+        # Two pointer approach makes it more optimal
+
+        # The amount of water stored is equal to height of the smallest bar of the two
+
+        res = 0
+        # Two pointers to move from the first and last index
+        l, r = 0, len(height) - 1
+        while l < r:
+            water_stored = min(height[l], height[r]) * (abs(l - r))
+            res = max(res, water_stored)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return res    
