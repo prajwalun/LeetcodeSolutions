c220e28e20d214fadd6e68c3da27e76cdc168893
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-04-18 18:24:35 -0700

    File updated at 18/04/2025, 18:24:34

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,4 +1,18 @@
-# Last updated: 18/04/2025, 18:15:32
+# Last updated: 18/04/2025, 18:24:34
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        # Using built in sort
+        # return sorted(s) == sorted(t)
+
+        # Using hash map count
+        if len(s) != len(t):
+            return False
+            
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        if countS == countT:
+            return True
+        return False
