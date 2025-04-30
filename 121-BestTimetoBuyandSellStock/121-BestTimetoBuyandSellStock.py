4ad0cb5a42c1f374c61de637aa73b1d57636b2ab
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-04-29 20:03:48 -0700

    File updated at 29/04/2025, 20:03:48

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,18 +1,14 @@
-# Last updated: 26/03/2025, 17:38:01
+# Last updated: 29/04/2025, 20:03:48
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        # Two Pointer Approach
         l, r = 0, 1
         maxP = 0
 
         while r < len(prices):
-            # Check the profit scenario
             if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxP = max(maxP, profit)
+                cur_profit = prices[r] - prices[l]
+                maxP = max(maxP, cur_profit)
             else:
-                # Update pointers
                 l = r
             r += 1
         return maxP
