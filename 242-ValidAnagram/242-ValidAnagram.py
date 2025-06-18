730f6f9ad2fa5b947bf209245209b039650ea482
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-06-18 13:55:49 -0700

    File updated at 18/06/2025, 13:55:49

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,20 +1,49 @@
-# Last updated: 24/04/2025, 12:50:55
+# Last updated: 18/06/2025, 13:55:49
 from collections import Counter 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Using built in sort
-        # return sorted(s) == sorted(t)
+        # # Using built in sort
+        # # return sorted(s) == sorted(t)
 
-        # Using hash map count
-        # TC: O(n) + O(n) + O(1)
-        # SC: O(k)  where k is number of elements 
+        # # Using hash map count
+        # # TC: O(n) + O(n) + O(1)
+        # # SC: O(k)  where k is number of elements 
+        # if len(s) != len(t):
+        #     return False
+        # # for i in range(len(s)):
+        # #     countS[s[i]] = 1 + countS.get(s[i], 0)
+        # #     countT[t[i]] = 1 + countT.get(t[i], 0)
+        # countS = Counter(s)
+        # countT = Counter(t)
+
+        # if countS == countT:
+        #     return True
+        # return False
+
+
+
+        # COunter
+
         if len(s) != len(t):
             return False
-
-        countS = Counter(s)
-        countT = Counter(t)
-
-        if countS == countT:
+        
+        if Counter(s) == Counter(t):
             return True
         return False
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
 
