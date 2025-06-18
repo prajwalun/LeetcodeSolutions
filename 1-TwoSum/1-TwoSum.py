ce3cb60b056c5cb4204545ce75dd72a8e9888437
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-06-18 14:30:48 -0700

    File updated at 18/06/2025, 14:30:48

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,12 +1,21 @@
-# Last updated: 18/06/2025, 14:21:20
+# Last updated: 18/06/2025, 14:30:48
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Brute force - TC: O(n^2)
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                currSum = nums[i] + nums[j]
-                if currSum == target:
-                    return [i,j]
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         currSum = nums[i] + nums[j]
+        #         if currSum == target:
+        #             return [i,j]
+
+        prevMap = {}
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[num] = i
+
         
                 
 
