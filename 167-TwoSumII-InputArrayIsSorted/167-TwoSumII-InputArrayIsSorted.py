6262193be980fb67d9dc43244f5e1058077eb8a5
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-06-19 12:32:48 -0700

    File updated at 19/06/2025, 12:32:47

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,17 +1,17 @@
-# Last updated: 28/04/2025, 23:12:52
+# Last updated: 19/06/2025, 12:32:47
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         new_sum = 0
         l, r = 0, len(numbers) - 1
-        while l < r:
 
+        while l < r:
             new_sum = numbers[l] + numbers[r]
             if new_sum < target:
                 l += 1
             elif new_sum > target:
                 r -= 1
             else:
-                return [l+1, r+1]
+                return [l + 1, r + 1]
         return []
 
     
