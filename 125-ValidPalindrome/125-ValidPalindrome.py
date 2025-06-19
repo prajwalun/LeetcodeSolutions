12485fff0d21c3919dc12ea7ab3a2524c54a37dc
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-06-19 12:25:06 -0700

    File updated at 19/06/2025, 12:25:05

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,4 +1,4 @@
-# Last updated: 28/04/2025, 22:52:43
+# Last updated: 19/06/2025, 12:25:05
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # Extra Space with new_str and also generating the reverse or new_str
@@ -15,19 +15,20 @@ class Solution:
 
         if not s:
             return False
-        l, r = 0, len(s) - 1
-        
-        while l < r:
-            while l < r and not self.alnum(s[l]):
-                l += 1
-            while r > l and not self.alnum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                    return False
-            l += 1
-            r -= 1
-        return True
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
 
+        return True
 
     def alnum(self, c):
         return (ord('A') <= ord(c) <= ord('Z') or
