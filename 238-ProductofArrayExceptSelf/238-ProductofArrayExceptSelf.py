27a7b3d04ff6d1dcec43b03ec91f722a5a28961a
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-06-18 20:43:54 -0700

    File updated at 18/06/2025, 20:43:54

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,8 +1,21 @@
-# Last updated: 24/04/2025, 15:27:42
+# Last updated: 18/06/2025, 20:43:54
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+        # res = [1] * (len(nums))
 
+        # prefix = 1
+        # for i in range(len(nums)):
+        #     res[i] = prefix
+        #     prefix *= nums[i]
+        # postfix = 1
+        # for i in range(len(nums) - 1, -1, -1):
+        #     res[i] *= postfix
+        #     postfix *= nums[i]
+        # return res
+
+        # Make all the elements 1 in the res arr
+        res = [1] * (len(nums))
+        # Set the first prefix which is 1
         prefix = 1
         for i in range(len(nums)):
             res[i] = prefix
