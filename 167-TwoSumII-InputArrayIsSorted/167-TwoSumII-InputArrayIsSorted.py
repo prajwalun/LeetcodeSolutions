5ca91e51776d7a881da983f436f3d84090ce0b5c
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-06-19 12:32:56 -0700

    File updated at 19/06/2025, 12:32:56

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,4 +1,4 @@
-# Last updated: 19/06/2025, 12:32:52
+# Last updated: 19/06/2025, 12:32:56
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         new_sum = 0
@@ -12,6 +12,6 @@ class Solution:
                 r -= 1
             else:
                 return [l + 1, r + 1]
-        r
+        return []
 
     
