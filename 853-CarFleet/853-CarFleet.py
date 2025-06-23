72dd12a19c467bedc895c01883b914694b242060
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-06-23 16:04:40 -0700

    Create 853-CarFleet

---
+++ 853-CarFleet/853-CarFleet.py
@@ -0,0 +1,13 @@
+# Last updated: 23/06/2025, 16:04:40
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p,s) for p,s in zip(position, speed)]
+        pair.sort(reverse=True)
+
+        stack = []
+
+        for p, s in pair:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
