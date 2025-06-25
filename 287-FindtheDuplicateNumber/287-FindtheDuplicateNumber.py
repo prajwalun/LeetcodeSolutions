e822a8a89f0fdbd324c80a0e41349f4e06e4bd0e
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-06-25 16:30:11 -0700

    Create 287-FindtheDuplicateNumber

---
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
@@ -0,0 +1,18 @@
+# Last updated: 25/06/2025, 16:30:10
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+            
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+        
