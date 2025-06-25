e68f3344fdfa54a991a5c875611e06a8f1adc808
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-06-25 16:02:22 -0700

    File updated at 25/06/2025, 16:02:21

--- 2-AddTwoNumbers/2-AddTwoNumbers.py
+++ 2-AddTwoNumbers/2-AddTwoNumbers.py
@@ -1,4 +1,4 @@
-# Last updated: 27/03/2025, 13:39:44
+# Last updated: 25/06/2025, 16:02:21
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -15,15 +15,11 @@ class Solution:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
 
-            # new digit
-            val = v1 + v2 + carry
-            # Find carry
-            carry = val // 10
-            # Find digit
-            val = val % 10
+            value = v1 + v2 + carry
+            carry = value // 10
+            val = value % 10
             cur.next = ListNode(val)
 
-            # Update the pointers
             cur = cur.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
