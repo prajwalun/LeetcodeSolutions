1a56d82735d1617d7e3e28aece033f98c798c7e8
Prajwal UN <60467926+prajwalun@users.noreply.github.com>
2025-07-04 14:35:21 -0700

    File updated at 04/07/2025, 14:35:20

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,4 +1,4 @@
-# Last updated: 04/07/2025, 14:35:16
+# Last updated: 04/07/2025, 14:35:20
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # for i in range(len(nums)):
@@ -14,4 +14,4 @@ class Solution:
             if diff in prevMap:
                 return [prevMap[diff], i]
             prevMap[n] = i
-        
+        return
